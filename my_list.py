4d6a7e1c2545d4c379d18e4575c24ad4ef5e7205
muiam <mwendwaharry01@gmail.com>
2024-03-02 08:43:05 +0300

    initial commit

---
+++ my_list.py
@@ -0,0 +1,27 @@
+# an empty list 
+my_list = []
+
+# Append the elements to my_list: 10, 20, 30, 40
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# we Insert the value 15 at the second position in the list
+my_list.insert(1, 15)
+
+# Extend my_list with another list whih is : [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+# Remove the last element from my_list using pop method
+my_list.pop()
+
+# Sort my_list in ascending order using sort method
+my_list.sort()
+
+# Find and print the index of the value 30 in my_list
+index_30 = my_list.index(30)
+print("Index of 30 in my_list:", index_30)
+
+# Print my_list
+print("Final my_list:", my_list)
